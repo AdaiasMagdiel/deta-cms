4b8105baf3c3261b863a855cc3c135411a595e01
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-04-08 17:34:01 -0300

    feat(repository): add drop method
    - Added a `drop` method to the `Repository` class.
    - The `drop` method fetches all items from the base and deletes them in batches.
    - This method helps in efficiently deleting all items from the base.

--- backend/app/entities/common/repository.py
+++ backend/app/entities/common/repository.py
@@ -8,3 +8,14 @@ class Repository:
     def __init__(self) -> None:
         self._deta = Deta(project_key=settings.DETA_PROJECT_KEY)
         self._base = self._deta.Base(self.table)
+
+    def drop(self):
+        res = self._base.fetch()
+        all_items = res.items
+
+        while res.last:
+            res = self._base.fetch(last=res.last)
+            all_items += res.items
+
+        for item in all_items:
+            self._base.delete(item['key'])
