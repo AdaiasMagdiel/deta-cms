4a0a9d40879a9f0b43a486e8cec1f60ce1dfa14c
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-04-08 17:34:52 -0300

    feat(user): add password hashing for user creation
    - Added `from typing import Any, Optional` to import statement
    - Added password hashing using `werkzeug.security.generate_password_hash`
    - Check for existing username and email before creating a user
    - Implemented `get_by` method to fetch user by key and value.

--- backend/app/entities/user/repository.py
+++ backend/app/entities/user/repository.py
@@ -1,8 +1,9 @@
 from datetime import UTC, datetime
-from typing import Any
+from typing import Any, Optional
 from deta import Deta, _Base
 from app.entities.common import Repository
 from app.entities.user import UserModel
+from werkzeug.security import generate_password_hash
 
 
 class UserRepository(Repository):
@@ -14,9 +15,22 @@ class UserRepository(Repository):
     def create(self, data: dict[str, Any]) -> UserModel:
         now = datetime.now(UTC)
 
-        data['key'] = None
         data['created_at'] = now.isoformat()
         data['updated_at'] = now.isoformat()
+
+        if 'key' not in data:
+            data['key'] = None
+
+        if 'password' in data:
+            data['password'] = generate_password_hash(data['password'])
+
+        by_username = self.get_by('username', data.get('username', ''))
+        if by_username is not None:
+            raise ValueError('The provided username is already in use.')
+
+        by_email = self.get_by('email', data.get('email', ''))
+        if by_email is not None:
+            raise ValueError('The provided email is already in use.')
 
         user = UserModel(**data)
 
@@ -24,3 +38,12 @@ class UserRepository(Repository):
         user.key = res['key']
 
         return user
+
+    def get_by(self, key: str, value: str) -> Optional[UserModel]:
+        res = self._base.fetch({key: value})
+
+        if res.count == 0:
+            return None
+
+        user = res.items[0]
+        return UserModel(**user)
