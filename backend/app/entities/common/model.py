cc03bc00f5d61ddc75bcbc6039d44207e6d24635
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-04-08 22:37:47 -0300

    feat(common): add base pydantic model
    - Added a new Pydantic BaseModel called `Model` in the common module
    - Included a `model_config` attribute of type `ConfigDict` with `validate_assignment=True`
    - Implemented an `update` method to update model attributes using keyword arguments

---
+++ backend/app/entities/common/model.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class Model(BaseModel):
+    model_config = ConfigDict(validate_assignment=True)
+
+    def update(self, **new_data):
+        for field, value in new_data.items():
+            setattr(self, field, value)
