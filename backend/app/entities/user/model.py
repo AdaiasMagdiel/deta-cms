de80de3c02089d45c5f06a9a689698cf406b3b15
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-04-08 22:35:24 -0300

    feat(user-model): add email and username validation
    - Added email validation method to ensure that the email field follows a valid format.
    - Added username validation method to ensure that the username field contains only letters, numbers, underscores, hyphens, and dots.

--- backend/app/entities/user/model.py
+++ backend/app/entities/user/model.py
@@ -1,8 +1,10 @@
+import string
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import field_validator
+from app.entities.common import Model
 
 
-class UserModel(BaseModel):
+class UserModel(Model):
     key: Optional[str]
     name: str
     username: str
@@ -11,3 +13,27 @@ class UserModel(BaseModel):
     role: str
     created_at: str
     updated_at: str
+
+    @field_validator('email')
+    @classmethod
+    def validate_email(cls, value: str) -> str:
+        if '@' not in value:
+            raise ValueError("Invalid email format")
+
+        if '.' not in value.split('@')[-1]:
+            raise ValueError("Invalid email format")
+
+        return value
+
+    @field_validator('username')
+    @classmethod
+    def validate_username(cls, value: str) -> str:
+        valid = string.ascii_letters + string.digits + '_-.'
+
+        for char in value:
+            if char not in valid:
+                raise ValueError(
+                    'Invalid username, the username should contain only letters, numbers and this digits: "_", "-" and "."'
+                )
+
+        return value
