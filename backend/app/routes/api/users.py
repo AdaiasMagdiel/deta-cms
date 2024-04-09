beb8241f1287cc8b0e9b44516f15e3b0aa1fce97
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-04-08 22:35:50 -0300

    feat(user): add create, get, and update user functionality
    - Added a new `create` function to handle user creation.
    - Added a `get` function to retrieve user information by user key.
    - Implemented an `update` function to update user information based on user key.
    - Improved error handling for user not found exceptions and validation errors.

--- backend/app/routes/api/users.py
+++ backend/app/routes/api/users.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, request
 from pydantic import ValidationError
 from app.entities.user import UserRepository
+from app.entities.user.exceptions import NotFoundException
 from app.entities.user.model import UserModel
 
 bp = Blueprint("users", __name__, url_prefix='/users')
 
 
 @bp.post("")
-def index():
+def create():
     data = request.get_json(silent=True) or {}
 
     try:
@@ -28,3 +29,40 @@ def index():
         }, 422
     except ValueError as e:
         return {'error': str(e)}, 422
+
+
+@bp.get("/<user_key>")
+def get(user_key: str):
+    repo = UserRepository()
+    user: UserModel = repo.get_by('key', user_key)
+
+    if user is None:
+        return {'error': 'User not found'}, 404
+
+    return {"user": user.model_dump(exclude={'password'})}, 200
+
+
+@bp.put("/<user_key>")
+def update(user_key: str):
+    data = request.get_json(silent=True) or {}
+
+    try:
+        repo = UserRepository()
+        user: UserModel = repo.update(user_key, data)
+
+        return user.model_dump(exclude={'password'}), 200
+    except ValidationError as e:
+        return {
+            'error':
+                'Validation Error',
+            'errors':
+                e.errors(
+                    include_context=False,
+                    include_input=False,
+                    include_url=False
+                )
+        }, 422
+    except ValueError as e:
+        return {'error': str(e)}, 422
+    except NotFoundException as e:
+        return {'error': str(e)}, 404
