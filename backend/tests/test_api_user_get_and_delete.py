cca35d5e73069c843d1c3db2d15b11b78bf6a9a9
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-04-09 19:22:27 -0300

    feat(api-pagination): add pagination feature to user retrieval
    - Implemented pagination feature for retrieving users in batches.
    - Added pagination support for fetching users with customizable page size.
    - Included tests to verify pagination functionality.

--- backend/tests/test_api_user_get_and_delete.py
+++ backend/tests/test_api_user_get_and_delete.py
@@ -1,9 +1,15 @@
 from random import randint
 from typing import Any
-
 from flask.testing import FlaskClient
-
+import pytest
 from app.entities.user import UserRepository
+
+
+@pytest.fixture(autouse=True)
+def run_after_each_test():
+    yield
+
+    UserRepository().drop()
 
 
 def create_user(client: FlaskClient) -> dict[str, Any]:
@@ -28,8 +34,6 @@ def test_get_user(client: FlaskClient):
 
     res = client.get(f'/api/users/{user["key"]}')
 
-    UserRepository().drop()
-
     assert res.status_code == 200
     assert (res.json or {})['user']['key'] == user['key']
 
@@ -37,6 +41,35 @@ def test_get_user(client: FlaskClient):
 def test_get_user_not_found(client: FlaskClient):
     res = client.get('/api/users/not-exists')
 
-    UserRepository().drop()
-
     assert res.status_code == 404
+
+
+def test_get_pagination(client: FlaskClient):
+    # total users: 10
+    for i in range(10):
+        create_user(client)
+
+    res = client.get('/api/users')  # default per page: 20
+    data = res.json or {}
+
+    assert res.status_code == 200
+    assert len(data['users']) == 10
+    assert data['last_page'] is True
+
+    # total users: 21
+    for i in range(11):
+        create_user(client)
+
+    res = client.get('/api/users')  # default per page: 20
+    data = res.json or {}
+
+    assert res.status_code == 200
+    assert len(data['users']) == 20
+    assert data['last_page'] is False
+
+    res = client.get('/api/users?page=2')
+    data = res.json or {}
+
+    assert res.status_code == 200
+    assert len(data['users']) == 1
+    assert data['last_page'] is True
