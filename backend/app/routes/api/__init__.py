99d7e11db966e534e17ad953850a2924fb00564b
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-07-16 21:03:17 -0300

    feat: Add role routes

--- backend/app/routes/api/__init__.py
+++ backend/app/routes/api/__init__.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
-from . import auth
+from . import auth, role
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
 
 def init_router(main: Blueprint):
     bp.register_blueprint(auth.bp)
+    bp.register_blueprint(role.bp)
 
     main.register_blueprint(bp)
