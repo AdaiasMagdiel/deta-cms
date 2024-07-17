ec91c270beedc8a2a129f24b6b206f4f7339272a
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-07-16 21:01:42 -0300

    feat: Add role model
    This commit introduces the Role model with the following changes:
    
    - Add the `Role` model to the `repositories` folder.
    - Add the `validate` method to the `Role` model to validate the data.
    - Add the `get_by` method to the `Role` model to retrieve a role by its key or value.
    - Add the `create` method to the `Role` model to create a new role.
    - Update the `Role` model to include the `key`, `title`, and `slug` fields.
    - Remove the `value` field from the `Role` model.
    - Add the `UniqueException` and `ValidateException` classes to the `repositories` folder.
    - Add the `init_db` method to the `Role` model to initialize the database connection.

--- backend/app/repositories/role.py
+++ backend/app/repositories/role.py
@@ -1,52 +1,71 @@
 from typing import Optional
 from deta import _Base
-from app.repositories import Repository
+from app.repositories import Base, UniqueException, ValidateException
 from app.utils import slugify
 
 
-class Role:
+class Role(Base):
     base: _Base
     table: str = "roles"
     ready: bool = False
-    fields: tuple = ('key', 'value', 'slug')
+    fields: tuple = ('key', 'title', 'slug')
 
     def __init__(
         self,
         key: Optional[str] = None,
-        value: Optional[str] = None,
+        title: Optional[str] = None,
         slug: Optional[str] = None,
     ):
         self.key = key
-        self.value = value
+        self.title = title
+
         if slug is None:
-            self.slug = slugify(value)
+            self.slug = slugify(title or "")
+        else:
+            self.slug = slug
 
         self.init_db()
 
-    @classmethod
-    def init_db(cls):
-        if cls.ready is False:
-            cls.base = Repository.get_deta().Base(cls.table)
-            cls.ready = True
+    def validate(self,
+                 data: dict[str, str],
+                 required: list[str] = []) -> dict[str, str]:
+        data = {k: v for k, v in data.items() if k in self.fields}
 
-        return cls
+        for k in required:
+            if k not in data or not data[k]:
+                raise ValidateException(f"The field '{k}' is required.")
 
-    def insert(self, data: dict[str, str]) -> 'Role':
+        if data['title'] == '':
+            raise ValidateException("The 'title' should be not empty.")
+
+        if 'title' in data and ('slug' not in data or data['slug'] == ''):
+            data['slug'] = slugify(data['title'])
+
+        return data
+
+    def get_by(self, key: str, value: str) -> Optional['Role']:
+        return super().base_get_by(key, value, Role)
+
+    def create(self, data: dict[str, str]) -> 'Role':
+        data = self.validate(data, required=['title'])
+
+        exists = self.get_by('slug', slugify(data['title']))
+        if exists:
+            raise UniqueException(
+                "Already exist a role with title '{0}'.".format(data['title'])
+            )
+
+        exists = self.get_by('slug', data['slug'])
+        if exists:
+            raise UniqueException(
+                "Already exists a role with slug '{0}'.".format(data['slug'])
+            )
+
         for k, v in data.items():
             if k in self.fields:
                 setattr(self, k, v)
 
-        if 'slug' not in data:
-            self.slug = slugify(self.value)
-
-        res: dict = self.base.put(self.to_dict())  #type:ignore
+        res: dict = self.base.put(self.to_dict())  # type:ignore
         self.key = res['key']
 
         return self
-
-    def to_dict(self) -> dict:
-        return {
-            k: v
-            for k, v in self.__dict__.items()
-            if not callable(v) and not k.startswith('__') and k in self.fields
-        }
